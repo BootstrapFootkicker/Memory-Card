b5a92fb8308f1706684e14804a5f85a3b9b84284
BootstrapFootkicker <ksealy93@gmail.com>
2024-03-13 18:47:49 -0400

    Grid Repsonsiveness
    Added responsive function to grid

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,15 @@ import "..//styles/card.css";
 export function Card({ sprite, name, types }) {
   return (
     <div className={"card"}>
-      {sprite && <img className={"sprite"} src={sprite} alt="pokemon" />}
-      <h1>{name}</h1>
-      <button onClick={() => console.log(types)}>Log Pokemon Data</button>
+      {sprite && (
+        <div className={"sprite-container"}>
+          <img className={"sprite"} src={sprite} alt="pokemon" />
+        </div>
+      )}
+      <div className={"card-info-container"}>
+        <h1>{name}</h1>
+        <button onClick={() => console.log(types)}>Log Pokemon Data</button>
+      </div>
     </div>
   );
 }
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -5,15 +5,19 @@ export function Grid({ pokemonData }) {
       <div className={"card-grid"}>
         {pokemonData.map((pokemon, index) => (
           <div className={"card"} key={index}>
-            <img
-              className={"sprite"}
-              src={pokemon.sprites.front_default}
-              alt="pokemon"
-            />
-            <h1>{pokemon.name}</h1>
-            <button onClick={() => console.log(pokemon.types)}>
-              Log Pokemon Data
-            </button>
+            <div className={"sprite-container"}>
+              <img
+                className={"sprite"}
+                src={pokemon.sprites.front_default}
+                alt="pokemon"
+              />
+              <div className={"card-info-container"}>
+                <h1>{pokemon.name}</h1>
+                <button onClick={() => console.log(pokemon.types)}>
+                  Log Pokemon Data
+                </button>
+              </div>
+            </div>
           </div>
         ))}
       </div>
