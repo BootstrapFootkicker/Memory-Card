92f3c39175c78650cd025c77342314d3f0611d2b
bootstrap <ksealy93@gmail.com>
2024-03-21 16:41:16 -0400

    TODOS
    Added todos
    Removed unused imports
    Added Comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
 import "..//styles/App.css";
-import { Card } from "./Card.jsx";
 import { DataHandler } from "./DataHandler.jsx";
 import { Grid } from "./Grid.jsx";
 
+
+//TODO Add way to randomize the pokemon data
+//TODO Add difficulty settings
+//TODO Add win screen
+//TODO add additional card styling like highlighting the card when hovered over
 export function App() {
   const pokemonData = DataHandler();
 
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -10,11 +10,13 @@ export function Grid({pokemonData}) {
     const [highScore, setHighScore] = useState(0);
     const[clicked, setClicked] = useState([]);
 
+    //shuffles the cards on mount and when the pokemon data changes
     useEffect(() => {
         shuffleCards();
     }, [pokemonData]);
 
     useEffect(() => {
+        //maps the pokemon data to the card component
         const newCardList = shuffledData.map((pokemon, index) => {
             return (
                 <Card
@@ -29,7 +31,6 @@ export function Grid({pokemonData}) {
 
         setCardList(newCardList);
     }, [shuffledData]);
-
     function cardClick(name) {
             if (clicked.includes(name)) {
                 if (score > highScore) {
